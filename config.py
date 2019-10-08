c827f586b35335d9583883b46d6e663e0ad0bc4d
Jordan Yoshihara <jordanyoshihara@gmail.com>
2019-10-08 11:46:10 -0700

    Added gitignore and structure config

---
+++ config.py
@@ -0,0 +1,42 @@
+SYMBOL_STRUCTURE = {
+  'name': 'symbols',
+  'fields': ['confidence', 'text'],
+  'objects': ['bounding_box', 'property']
+}
+
+WORD_STRUCTURE = {
+  'name': 'words',
+  'fields': ['confidence'],
+  'objects': ['bounding_box', 'property'],
+  'list': SYMBOL_STRUCTURE
+}
+
+PARAGRAPH_STRUCTURE = {
+  'name': 'paragraphs',
+  'fields': ['confidence'],
+  'objects': ['bounding_box', 'property'],
+  'list': WORD_STRUCTURE
+}
+
+BLOCK_STRUCTURE = {
+  'name': 'blocks',
+  'fields': ['block_type', 'confidence'],
+  'objects': ['bounding_box', 'property'],
+  'list': PARAGRAPH_STRUCTURE
+}
+
+PAGE_STRUCTURE = {
+  'name': 'pages',
+  'fields': ['confidence', 'height', 'width'],
+  'objects': ['property'],
+  'list': BLOCK_STRUCTURE
+}
+
+STRUCTURE = {
+  'name': 'overall',
+  'fields': ['text'],
+  'objects': [],
+  'list': PAGE_STRUCTURE
+}
+
+WRITE_DIRECTORY = 'scans'
