8455a48a8bafd378d9f4b0fc840f61491499a12b
kollivier <kevino@theolliviers.com>
2019-10-15 19:45:29 -0700

    Update to use current folder structure.

--- scanner.py
+++ scanner.py
@@ -46,6 +46,7 @@ class CurriculumScanner(object):
     file_id = get_hash(path)
     filename, _ext = os.path.splitext(os.path.basename(self.path))
     self.directory = "{}-{}".format(filename, file_id)
+    self.index_dir = os.path.sep.join([WRITE_DIRECTORY, self.directory])
     self.pages = self.load()
 
   @classmethod
@@ -63,7 +64,7 @@ class CurriculumScanner(object):
         Args: None
         Returns dict of index.json data
     """
-    index_path = os.path.sep.join([WRITE_DIRECTORY, self.directory, 'index.json'])
+    index_path = os.path.sep.join([self.index_dir, 'index.json'])
     if not os.path.exists(index_path):
       raise RuntimeError('index.json file not found for {}. Please run CurriculumScanner.process(filepath) and try again'.format(self.path))
     with open(index_path, 'rb') as fobj:
@@ -75,7 +76,8 @@ class CurriculumScanner(object):
         Args: page_number (int) page to read data from
         Returns dict of page data
     """
-    with open(self.pages[page_number]['file'], 'rb') as fobj:
+
+    with open(os.path.join(self.index_dir, self.pages[page_number]['file']), 'rb') as fobj:
       return json.load(fobj)
 
   def get_page_image(self, page_number):
@@ -84,7 +86,7 @@ class CurriculumScanner(object):
         Args: page_number (int) page to get image for
         Returns PIL.Image for page
     """
-    return Image.open(self.pages[page_number]['image'])
+    return Image.open(os.path.join(self.index_dir, self.pages[page_number]['image']))
 
 
   def get_next_page(self):
@@ -185,9 +187,8 @@ class CurriculumScanner(object):
       Paragraphs = blue
       Words = yellow
     """
-    filepath = self.pages[page_number]['image']
     page_data = self.get_page_data(page_number)
-    image = Image.open(filepath)
+    image = self.get_page_image(page_number)
     for page in page_data['pages']:
       for block in page['blocks']:
         for paragraph in block['paragraphs']:
@@ -245,6 +246,7 @@ class CurriculumScanner(object):
                   "bounding_box": word['bounding_box']
                 })
             if not word_found and text in paragraph['text']:
+                print("paragraph text = {}".format(paragraph['text']))
                 results.append({
                   "page": page_number,
                   "block": block_index,
